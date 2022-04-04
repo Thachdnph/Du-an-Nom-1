09a72078b06da1333a213c05bf0ddb5214c1f617
Đặng Ngọc Thạch <100736264+Thachdnph@users.noreply.github.com>
2022-04-04 09:52:26 +0700

    create

---
+++ app/src/main/java/vn/poly/quanlybanhang/Activity/SplashScreenActivity.java
@@ -0,0 +1,64 @@
+package vn.poly.quanlybanhang.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import vn.poly.quanlybanhang.GioiThieuActivity;
+import com.example.duan1android.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private ImageView imglogo,fly;
+
+    TextView tvLoading;
+    AnimationDrawable rocketAnimation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash_screen);
+        imglogo = findViewById(R.id.imageSplashScrenn);
+        fly=findViewById(R.id.imgFlySplashScrenn);
+        tvLoading= findViewById(R.id.tvSplashScren);
+        startAnimation();
+        fly.setBackgroundResource(R.drawable.fly);
+        rocketAnimation = (AnimationDrawable) fly.getBackground();
+        rocketAnimation.start();
+
+    }
+    private void startAnimation() {
+        Animation rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        Animation translate = AnimationUtils.loadAnimation(this, R.anim.translate);
+        rotate.reset();
+        translate.reset();
+
+        imglogo.setAnimation(rotate);
+        fly.setAnimation(translate);
+        tvLoading.setAnimation(translate);
+        Thread mThread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                int waited = 0;
+                while (waited < 4000) {
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    waited += 100;
+                }
+                SplashScreenActivity.this.finish();
+                Intent intent = new Intent(SplashScreenActivity.this, GioiThieuActivity.class);
+                startActivity(intent);
+            }
+        };
+        mThread.start();
+    }
+}
